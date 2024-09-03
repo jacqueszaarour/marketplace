1e7698fe5fac5e3dcb35442d5ba91b9e3a9410be
jacqueszaarour <jwzaarour@gmail.com>
2024-09-03 19:42:52 -0400

    removing apmlify

--- amplify/backend/function/cartHandler/src/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-
-export const cartHandler = defineFunction({
-  name: "cart-handler",
-  entry: "./handler.ts",
-});
--- amplify/backend/function/productHandler/src/resource.ts
+++
@@ -1,6 +0,0 @@
-import { defineFunction } from "@aws-amplify/backend";
-
-export const productHandler = defineFunction({
-  name: "product-handler",
-  entry: "./handler.ts",
-});
